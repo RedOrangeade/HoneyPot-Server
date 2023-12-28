279b4610ed55553662783c3371b23572601fac89
taeyeon <tyeon42051@naver.com>
2023-12-28 15:54:11 +0900

    기본설정

---
+++ src/test/java/com/grapefruit/honeypot/HoneyPotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grapefruit.honeypot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoneyPotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
